14f2e115a2dc99bb03f0cfd7a13dfa1aa510594a
Eren1831 <erenwtp09@gmail.com>
2017-03-26 17:52:29 +0300

    Update robot.ino

--- Arduino/robot/robot.ino
+++ Arduino/robot/robot.ino
@@ -1,11 +1,29 @@
 
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
 
+ros::NodeHandle nh;
+
+std_msgs::Float32MultiArray angulos;
+ros::Publisher ext("node", &value);
+
+float veri;
 void setup()
 {
-Serial.begin(9600);	
+  nh.initNode();
+  nh.advertise(ext);
+  pinMode(A0,INPUT);
 }
-
 void loop()
 {
-	Serial.println("DENEME-EREN");
+  veri=analogRead(A0);
+  angulos.data[0]=veri
+  nh.spinOnce();
+  ext.publish(& angulos);
+  delay(500);
 }
