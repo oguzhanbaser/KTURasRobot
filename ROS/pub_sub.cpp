f32035aee5136fa37b9a53a98efff5c8ef8ede2a
Eren1831 <erenwtp09@gmail.com>
2017-05-02 14:35:03 +0300

    Update pub_sub.cpp

--- ROS/pub_sub.cpp
+++ ROS/pub_sub.cpp
@@ -4,25 +4,18 @@
 #include <string>
 
 using namespace std;
-std::string a; 
-std_msgs::String sendValue;
-//char* tempValue = new char [3];
+
 void chatterCallback(const std_msgs::String::ConstPtr& value)
 
 {
-
   ROS_INFO("GelenVeri: [%s]", value->data.c_str());
-  ROS_INFO("GelenVeri:EREN");
-  //sendValue.data = value->data;
-  //tempValue[0]=value->data.substr(0,3);
-  //tempValue[1]=value->data.substr(3,6);
-  //tempValue[2]=value->data.substr(6,9);
-
 }
 
 
 int main(int argc, char **argv)
 {
+  
+  
   ros::init(argc, argv, "listener");
 
   ros::NodeHandle n;
@@ -31,25 +24,21 @@ int main(int argc, char **argv)
 
   ros::Publisher list = n.advertise<std_msgs::String>("list", 1000);
 
-  ros::Rate loop_rate(10);
-
- 
- 
-  //ROS_INFO("%d", msg.data);
-    
-
+  ros::Rate loop_rate(10);  
+   
   while (ros::ok())
   {
 
     std_msgs::String msg;
     std::stringstream ss;
-    ss << "111";
+    ss << "255"; //gönderilen pwm degeri
     msg.data = ss.str();
-   // ROS_INFO("%s", msg.data.c_str());
+   // ROS_INFO("%s", msg.data.c_str()); //Gönderilen degeri terminalde görme
     list.publish(msg);
     ros::spinOnce();
     loop_rate.sleep();
 
   }
+  
   return 0;
 }
