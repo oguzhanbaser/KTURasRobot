2e357b42e7302cf9769a384939d6656d52acff2d
Eren1831 <erenwtp09@gmail.com>
2017-05-02 14:40:09 +0300

    Update arduino_pub_sub.ino

--- Arduino/arduino_pub_sub.ino
+++ Arduino/arduino_pub_sub.ino
@@ -1,17 +1,19 @@
-
 #include <ros.h>
 #include <std_msgs/String.h>
-ros::NodeHandle  nh;
 
+ros::NodeHandle  nh;
 std_msgs::String str_msg;
 
 String inString;
-String sendSerial;
+String sendSerial; // Serial. monitör
 String sendSens;
-char sendValue[10];
+
+char sendValue[10]; // Serial Monitör
 char sendSensor[50];
+
 String dizi[] = {"value1", "value2", "value3", "value4", "value5", "value6"};
 int analog[] = {A0, A1, A2, A3, A4, A5};
+unsigned long zaman=0;
 void Veri( const std_msgs::String& cmd_msg) {
   sendSerial = cmd_msg.data;
   inString = cmd_msg.data;
@@ -25,27 +27,39 @@ ros::Publisher talk("chatter", &str_msg);
 
 void setup() {
   pinMode(11, OUTPUT);
+
   for (int i = 0; i < 5; i++)
   {
     pinMode(analog[i], INPUT);
   }
-
+  
   nh.initNode();
   nh.subscribe(list);
   nh.advertise(talk);
+
 }
 
 void loop() {
-  sendSerial.toCharArray(sendValue, 10);
+  
+
   for (int i = 0; i < 6; i++)
   {
     dizi[i] = String(255);
   }
+  
   sendSens = "#" + dizi[0] + "|" + dizi[1] + "|" + dizi[2] + "|" + dizi[3] + "|" + dizi[4] + "|" + dizi[5] ;
+  if(millis() - zaman >1000)
+  {
   sendSens.toCharArray(sendSensor, 50);
-  str_msg.data = sendValue;
+  sendSerial.toCharArray(sendValue, 10);
+  zaman = millis();
+  }
+  // str_msg.data = sendValue; Serial Monitör için
   str_msg.data = sendSensor;
+  
   talk.publish( &str_msg );
   nh.spinOnce();
-  delay(100);
+  
+  
+  return 0;
 }
